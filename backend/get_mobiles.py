153292ac7252ef3d80ccdb97d0d51e471a304ffc
Aryan Gupta <aryangupta07075@gmail.com>
2023-09-22 14:57:42 +0530

    gets all the mobiles on flipkart with  price

--- backend/get_mobiles.py
+++ backend/get_mobiles.py
@@ -7,16 +7,23 @@ def fetch_data_and_save_to_file(url, path):
     with open(path, "w", encoding="utf-8") as f:
         f.write(data.text)
 
-
 url = "https://www.flipkart.com/search?q=mobiles&as=on&as-show=on&otracker=AS_Query_TrendingAutoSuggest_3_0_na_na_na&otracker1=AS_Query_TrendingAutoSuggest_3_0_na_na_na&as-pos=3&as-type=TRENDING&suggestionId=mobiles&requestId=90c188d0-bf6a-4593-b462-f5fdb8d0fa73"
-file_name = "mobiles.html"
-fetch_data_and_save_to_file(url, file_name)
-with open(file_name, "r", encoding="utf-8") as f:
-    html_doc = f.read()
-soup = BeautifulSoup(html_doc, "html.parser")
-mobile_titles = soup.find_all("div", {"class": "_4rR01T"})
-prices = soup.find_all("div", {"class": "_30jeq3 _1_WHN1"})
-for mobile_title, mobile_price in zip(mobile_titles, prices):
-    title = mobile_title.text
-    price = mobile_price.text
-    print(title, price)
+x = 1
+while True:
+
+    file_name = f"mobiles{x}.html"
+    try:
+        fetch_data_and_save_to_file(url + f"&page={x}", file_name)
+    except Exception:
+        break
+
+    with open(file_name, "r", encoding="utf-8") as f:
+        html_doc = f.read()
+    soup = BeautifulSoup(html_doc, "html.parser")
+    mobile_titles = soup.find_all("div", {"class": "_4rR01T"})
+    prices = soup.find_all("div", {"class": "_30jeq3 _1_WHN1"})
+    for mobile_title, mobile_price in zip(mobile_titles, prices):
+        title = mobile_title.text
+        price = mobile_price.text
+        print(title, price)
+    x += 1
