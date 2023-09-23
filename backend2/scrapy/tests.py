da10ebf60d9616c406416bdb34a9718d02c22789
Aryan Gupta <aryangupta07075@gmail.com>
2023-09-23 22:08:37 +0530

    fixed errors

--- backend2/scrapy/tests.py
+++ backend2/scrapy/tests.py
@@ -1,3 +1,58 @@
 from django.test import TestCase
+from unittest.mock import patch
+from scrapy.views import DisplayData
 
-# Create your tests here.
+class DisplayDataTestCase(TestCase):
+    @patch('scrapy.views.fetch_amazon_data')
+    @patch('scrapy.views.fetch_flipkart_data')
+    def test_display_data_success(self, mock_fetch_flipkart_data, mock_fetch_amazon_data):
+        # Mock the fetch_amazon_data and fetch_flipkart_data functions
+        mock_fetch_amazon_data.return_value = {
+            "title": "Amazon Product Title",
+            "price": 100.00,
+            "image": "amazon_image_url",
+            "url": "amazon_product_url",
+        }
+        mock_fetch_flipkart_data.return_value = {
+            "title": "Flipkart Product Title",
+            "price": 90.00,
+            "image": "flipkart_image_url",
+            "url": "flipkart_product_url",
+        }
+
+        # Make a GET request to the view
+        response = self.client.get('/your-api-url/')
+
+        # Check that the response status code is 200
+        self.assertEqual(response.status_code, 200)
+
+        # Check the content of the response
+        expected_response = {
+            "status": 200,
+            "amazon": {
+                "title": "Amazon Product Title",
+                "price": 100.00,
+                "image": "amazon_image_url",
+                "url": "amazon_product_url",
+            },
+            "flipkart": {
+                "title": "Flipkart Product Title",
+                "price": 90.00,
+                "image": "flipkart_image_url",
+                "url": "flipkart_product_url",
+            }
+        }
+        self.assertEqual(response.data, expected_response)
+
+    @patch('scrapy.views.fetch_amazon_data')
+    @patch('scrapy.views.fetch_flipkart_data')
+    def test_display_data_failure(self, mock_fetch_flipkart_data, mock_fetch_amazon_data):
+        # Mock the fetch_amazon_data and fetch_flipkart_data functions to raise an exception
+        mock_fetch_amazon_data.side_effect = Exception("Amazon Error")
+        mock_fetch_flipkart_data.side_effect = Exception("Flipkart Error")
+
+        # Make a GET request to the view
+        response = self.client.get('/your-api-url/')
+
+        # Check that the response status code is 404
+        self.assertEqual(response.status_code, 404)
--- backend2/scrapy/views.py
+++ backend2/scrapy/views.py
@@ -13,7 +13,7 @@ class MarketPlace:
         self.desc = desc
 
     def __repr__(self):
-        print(f"{self.title}{self.price}{self.url}{self.image}{self.desc}")
+        return f"{self.title}{self.price}{self.url}{self.image}{self.desc}"
 
 
 Headers = {
@@ -32,56 +32,75 @@ flipkart = MarketPlace()
 croma = MarketPlace()
 
 def fetch_flipkart_data(product_name):
-    base_url = "https://www.flipkart.com/search?q="
-    url = base_url + product_name
-    data = requests.get(url, headers=Headers)
-    soup = BeautifulSoup(data.content, 'lxml')
-    mobile_titles = soup.find_all("div", {"class": "_4rR01T"})
-    prices = soup.find_all("div", {"class": ["_30jeq3", "_1_WHN1"]})
+    try:
+        web_url = "https://www.flipkart.com"
+        base_url = "https://www.flipkart.com/search?q="
+        url = web_url + product_name
+        data = requests.get(url)
+        soup = BeautifulSoup(data.content, 'lxml')
+        mobile_titles = soup.find_all("div", {"class": "_4rR01T"})
+        prices = soup.find_all("div", {"class": ["_30jeq3", "_1_WHN1"]})
 
-    image_srcs_div = soup.find_all("div", {"class": "CXW8mj"})
-    product_links = soup.find_all("a", {"class": "_1fQZEK"})
-    for mobile_title, mobile_price, image_src_div, product in zip(mobile_titles, prices, image_srcs_div, product_links):
-        url = product["href"]
-        product_url = base_url + url
-        image = image_src_div.find("img")
-        image_src = image["src"]
-        title = mobile_title.text
-        price = mobile_price.text
+        image_srcs_div = soup.find_all("div", {"class": "CXW8mj"})
+        product_links = soup.find_all("a", {"class": "_1fQZEK"})
+        for mobile_title, mobile_price, image_src_div, product in zip(mobile_titles, prices, image_srcs_div, product_links):
+            url = product["href"]
+            print(url)
+            product_url = base_url + url
+            image = image_src_div.find("img")
+            image_src = image["src"]
+            title = mobile_title.text
+            price = mobile_price.text
 
-        break
-    return title, price, image_src, product_url
-
+            break
+        return title, price, image_src, product_url
+    except Exception:
+        title = "None"
+        price = "None"
+        image_src = "None"
+        product_url = "None"
+        return title, price, image_src, product_url
 
 def fetch_amazon_data(product_name):
-    base_url = "https://www.amazon.in"
-    url = "https://www.amazon.in/s?k=" + product_name
-    data = requests.get(url, headers=Headers)
-    soup = BeautifulSoup(data.content, 'lxml')
-    image_div = soup.find("img", {"class": "s-image"})
-    src = image_div["src"]
-    # print(src)
-    get_div = soup.find("div", attrs={"class": "sg-col-inner"})
+    try:
+        base_url = "https://www.amazon.in"
+        url = "https://www.amazon.in/s?k=" + product_name
+        data = requests.get(url, headers=Headers)
+        # print("Response is : ", data.content)
+        soup = BeautifulSoup(data.content, 'lxml')
+        image_div = soup.find("img", {"class": "s-image"})
+        src = image_div["src"]
+        # print(src)
+        get_div = soup.find("div", attrs={"class": "sg-col-inner"})
 
-    mobile_titles = get_div.find_all("span", {"class": ["a-color-state", "a-text-bold"]})
-    product_url = soup.find("a", attrs={"class": ["a-link-normal", "s-underline-text", "s-underline-link-text"]})
-    url = product_url["href"]
-    complete_url = base_url + url
-    prices = soup.find_all("span", {"class": "a-price-whole"})
-    # print(prices)
-    for mobile_title, mobile_price in zip(mobile_titles, prices):
-        title = mobile_title.text
-        price = mobile_price.text
+        mobile_titles = get_div.find_all("span", {"class": ["a-color-state", "a-text-bold"]})
+        product_urls_h2 = soup.find_all("h2", {"class": ["a-size-mini", "a-spacing_none", "a-color-base", "s-line-clamp-2"]})
 
-        break
+        # complete_url = base_url + product_url["href"]
+        prices = soup.find_all("span", {"class": "a-price-whole"})
+        # print(prices)
+        for mobile_title, mobile_price, product_url_h2 in zip(mobile_titles, prices, product_urls_h2):
+            product_url = product_url_h2.a["href"]
+            complete_url = base_url + product_url
+            title = mobile_title.text
+            price = mobile_price.text
 
-    return title, price, src, complete_url
+            break
+
+        return title, price, src, complete_url
+    except Exception:
+        title = "None"
+        price = "None"
+        src = "None"
+        complete_url = "None"
+        return title, price, image_src, product_url
 
 class DisplayData(APIView):
 
 ## This is currently for testing
     def get(self, request):
-        product_name = "Apple iPhone 15 Plus (128 GB) - Blue"
+        product_name = "HP Victus AMD Ryzen 7-5800H"
+
         try:
             amazon.title, amazon.price, amazon.image, amazon.url = fetch_amazon_data(product_name)
             flipkart.title, flipkart.price, flipkart.image, flipkart.url = fetch_flipkart_data(product_name)
@@ -112,8 +131,8 @@ class DisplayData(APIView):
     def post(self, request):
         product_name = request.POST["product-name"]
         amazon_title, amazon_price = fetch_amazon_data(product_name)
-        # flipkart_title, flipkart_price, image_src = fetch_flipkart_data(product_name)
+        flipkart_title, flipkart_price, image_src = fetch_flipkart_data(product_name)
         return Response({
             "Amazon": {"title": amazon_title, "price": amazon_price},
-            # "Flipkart": {"title": flipkart_title, "price": flipkart_price, "img src": image_src}
+            "Flipkart": {"title": flipkart_title, "price": flipkart_price, "img src": image_src}
         })
